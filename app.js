5b7e934d4318732fb54da4d77b2f222b55c77c79
himanshu <himanshuverma.cybernext@gmail.com>
2022-11-17 17:08:50 +0530

    added handlebars for rendering

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const exphbs = require('express-handlebars');
 const app = express();
 require('dotenv').config();
 require('./config/db')
 
 const PORT = process.env.PORT || 4000
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs.engine({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
 //middleware
 app.use(cors());
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
+app.get('/',(req,res)=> res.render('index'));
 app.use(require('./routes'));
 
+// Homepage Route
 
 /* //routing
 function middleware(req,res,next){
